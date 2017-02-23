48f7c0b3dc36714fbaa76b935a59d03566faf1d5
aviklar10 <aviklar10@gmail.com>
2017-02-23 10:14:30 +0200

    first youtube search .. using functional component

--- components/search_bar.js
+++ components/search_bar.js
@@ -1,9 +1,30 @@
 /**
  * Created by aviklar10 on 22/02/2017.
  */
-import React from 'react';
-const SearchBar = () => {
-    return <input />;// we need the import react since weback transform this line to ->React.createElement
-};
+import React, {Component} from 'react';
+
+class SearchBar extends Component { // extends Component gives me the Component abilities
+    constructor(props) {
+        super(props);
+        this.state = {term: ''};
+    }
+    //when user prints something on the screen the following occurs:
+    /*  1. onChange event occurs
+    *   2. it updates the component state into the term value
+    *   3. because of the setState render method is called again and now value keep the previous printed value and then
+     *   input is bar is updated with the change*/
+    render() {
+        return (
+
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({term: event.target.value})}/>
+            </div>
+        )
+
+    }
+
+}
 
 export default SearchBar;
--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-//
-// import App from './components/app';
-// import reducers from './reducers';
-//
-// const createStoreWithMiddleware = applyMiddleware()(createStore);
-//
-// ReactDOM.render(
-//   <Provider store={createStoreWithMiddleware(reducers)}>
-//     <App />
-//   </Provider>
-//   , document.querySelector('.container'));
+import YTSearch from 'youtube-api-search'
+const API_KEY = 'AIzaSyAlMRjqARiYMWf4yxbaJ9Ns8p2LlHn43ks	';
 
-const API_KEY = 'AIzaSyDNupdkr66H6vQBT4VTkU2HPHyfYkTazcM';
 
+YTSearch({key:API_KEY, term: 'surfboards'}, function (data) {
+    console.log(data);
+
+});
 // create new component which should produce html
 const App = () => {
     return (
